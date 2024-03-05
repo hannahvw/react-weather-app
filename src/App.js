94393ac46ee7e86d1cb8a4bbad8b16e54a17f4f3
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-03-05 14:08:18 -0600

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       Weather App
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/hannahvw/react-weather-app" target="_blank">
+          Hannah Van Wyk
+        </a>{" "}
+        and is open-sourced on GitHub.
+      </p>
     </div>
   );
 }
