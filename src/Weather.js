05694c33527ae0cb613dfce3d43e1006ed07f939
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-03-06 15:40:40 -0600

    Added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form className="row">
-        <div className="col-9">
-          <input
-            type="text"
-            placeholder="Enter a city"
-            autoFocus="on"
-            className="form-control"
-          />
-        </div>
-        <div className="col-3">
-          <button className="btn btn-info w-100">Search</button>
-        </div>
-      </form>
-      <h1>Chicago</h1>
-      <ul>
-        <li>Wednesday 10:45</li>
-        <li>Mostly Clear</li>
-      </ul>
-      <div className="row current-weather">
-        <div className="col-6">
-          <div className="current-temp">
-            <img
-              src="https://duckduckgo.com/assets/weather/icons/weatherkit/MostlyClear.svg"
-              alt="Mostly Clear"
+export default function Weather(props) {
+  let [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      iconUrl:
+        "https://duckduckgo.com/assets/weather/icons/weatherkit/MostlyClear.svg",
+      date: "Wednesday 10:45",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form className="row">
+          <div className="col-9">
+            <input
+              type="text"
+              placeholder="Enter a city"
+              autoFocus="on"
+              className="form-control"
             />
-            <span className="temperature">32</span>
-            <span className="unit">°F</span>
+          </div>
+          <div className="col-3">
+            <button className="btn btn-info w-100">Search</button>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row current-weather">
+          <div className="col-6">
+            <div className="current-temp">
+              <img src={weatherData.iconUrl} alt={weatherData.description} />
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">°F</span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} mph</li>
+            </ul>
           </div>
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: 71%</li>
-            <li>Wind: 6 mph</li>
-          </ul>
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `2cacbf3044aeb9a87b5a33at06fco72a`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+    return <div>loading...</div>;
+  }
 }
